885876176c9058774f2847b0313cb06d7884a44c
Priyanshu <priyanshupatel405@gmail.com>
2025-06-03 00:11:05 +0530

    Feat:..

--- client/src/app/Components/Projects.tsx
+++ client/src/app/Components/Projects.tsx
@@ -41,12 +41,12 @@ const projects: Project[] = [
 
 const Projects: React.FC = () => {
   return (
-    <section id="projects" className="py-12 px-6 md:px-16 mb-6 mt-[-14vmin] bg-white text-teal-300">
-      <h2 className="text-3xl font-bold text-center mb-10 ">Projects</h2>
+    <section id="projects" className="py-10 mx-auto px-6 mb-6 mt-[-14vmin] bg-white text-teal-300">
+      <h2 className="text-4xl font-bold text-teal-300 text-center mb-6 ">Projects</h2>
        <div>
             <hr className="w-full mx-auto border-t-2 border-teal-300 mb-10" />
           </div>
-      <div className="grid md:grid-cols-2 gap-6">
+      <div className="grid md:grid-cols-2 gap-6  py-1 px-4">
         {projects.map((project, index) => (
           <div key={index}  className={`bg-white p-6 border-black border-1 rounded-lg shadow-lg transition-all duration-300 ease-in-out transform opacity-100 translate-y-0 hover:shadow-xl hover:bg-blue-50 hover:translate-y-[-2vmin] hover:shadow-xl hover:shadow-blue-200 hover:border-blue-100 hover:scale-103`}
           >
